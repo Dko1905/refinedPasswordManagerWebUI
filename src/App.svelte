362ba7c3448ebf52301448d38c025a38fdf1dfda
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-03-19 13:15:45 +0100

    Added login page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,20 @@
 <script lang="ts">
-	import {Account} from './entities/account'
-	import {Credential} from './entities/credential'
-	import {
-		getAccountRepository,
-		getCredentialRepository,
-		getTokenService
-	} from './provider'
+	import Login from './pages/Login.svelte'
 
-	let click: () => void, add: () => void, del: () => void, put: () => void
-	let inputStr: string
-
-		// Global async
-	;(async () => {
-		const aRepo = await getAccountRepository()
-		const cRepo = await getCredentialRepository()
-		const tokenService = await getTokenService()
-
-		const account = new Account(null, 'example', 'example', null)
-		const id = await tokenService.getAccountId(account)
-		const cred = new Credential(null, id, 'google.com', '123', '123', '1')
-
-		cRepo.setTokenCallback(() => tokenService.login(account))
-		aRepo.setTokenCallback(() => tokenService.login(account))
-
-		click = () => {
-			cRepo.getCredentials().then(console.log)
-		}
-		add = () => {
-			cRepo.addCredential(cred).then(console.log)
-		}
-		del = () => {
-			cRepo.deleteCredential(Number.parseInt(inputStr)).then(console.log)
-		}
-		put = () => {
-			cRepo
-				.updateCredential(
-					new Credential(Number.parseInt(inputStr), id, 'web', 'u', 'p', 'ex')
-				)
-				.then(console.log)
-		}
-	})()
+	const test = (e) => {
+		console.log(e)
+	}
 </script>
 
-<div class="App">
-	<button on:click={click}>Click me!</button>
-	<button on:click={add}>Add</button>
-	<button on:click={del}>Delete</button>
-	<input type="text" bind:value={inputStr} />
-	<button on:click={put}>Update</button>
+<div class="app">
+	<Login on:login={test} />
 </div>
+
+<style>
+	.app {
+		width: 100%;
+		height: 100%;
+		padding: 0;
+		margin: 0;
+	}
+</style>
---
+++ src/pages/Login.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import {createEventDispatcher} from 'svelte'
+
+	const dispatch = createEventDispatcher();
+
+	let username: string = ''
+	let password: string = ''
+	let epassword: string = ''
+	let cache: boolean = true
+	let apiversion: string = 'v3'
+	const formSubmit = (e: Event) => {
+		dispatch('login', {
+			username, password, epassword, cache, apiversion
+		})
+	}
+</script>
+
+<div id="container">
+	<form on:submit|preventDefault={formSubmit} id="form">
+		<input type="text" bind:value={username} placeholder="username" /><br />
+		<input type="text" bind:value={password} placeholder="password" /><br />
+		<input type="text" bind:value={epassword} placeholder="epassword" /><br />
+		<div id="checkBoxes">
+			<label for="cacheLogin">Cache session:</label>
+			<input type="checkbox" bind:checked={cache} id="cacheLogin" name="cacheLogin" />
+		</div>
+		<div class="versContainer">
+			<label for="apiversion">Api version:</label>
+			<select bind:value={apiversion} name="apiversion" id="apiversion">
+				<option value="v2">v2</option>
+				<option default value="v3">v3</option>
+			</select>
+		</div>
+		<div class="btnContainer">
+			<input type="submit" value="Login" />
+		</div>
+	</form>
+</div>
+
+<style>
+	#container {
+		position: absolute;
+		left: 50%;
+		top: 40%;
+		-ms-transform: translateY(-50%) translateX(-50%);
+		transform: translateY(-50%) translateX(-50%);
+	}
+	#form {
+		background-color: blue;
+		color: white;
+		margin: 10px;
+		padding: 7px;
+		border-radius: 5px;
+	}
+	#form * {
+		margin: 2px;
+	}
+	#form .btnContainer {
+		text-align: center;
+	}
+	#form .btnContainer input {
+		background-color: green;
+		color: white;
+		border-radius: 5px;
+		border: thin solid white;
+		padding: 4px;
+	}
+	#form .btnContainer input:active {
+		background-color: darkgreen;
+	}
+	#form #checkBoxes input {
+		display: inline-block;
+		padding-right: 10px;
+		white-space: nowrap;
+	}
+	#form #checkBoxes input {
+		vertical-align: middle;
+	}
+	.versContainer select {
+		border: 0;
+	}
+</style>
